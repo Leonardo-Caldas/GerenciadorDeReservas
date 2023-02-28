2208d14eefd896080565e9069953ff8985233fdd
lolzeen <leonardo_nogueira13@hotmail.com>
2023-02-28 20:13:10 -0300

    Remocao do objeto telefone

--- src/main/java/com/gerenciador/dto/cliente/Cliente.java
+++ src/main/java/com/gerenciador/dto/cliente/Cliente.java
@@ -1,11 +1,9 @@
 package com.gerenciador.dto.cliente;
 
-import com.gerenciador.dto.telefone.Telefone;
-
 public interface Cliente {
     String nome = null;
     String CPF = null;
-    Telefone telefone = null;
+    String telefone = null;
 
 
 
