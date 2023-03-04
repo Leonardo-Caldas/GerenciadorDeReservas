c210bd669b2c6ee96f3f11f9d6566269b8a23e4d
lolzeen <leonardo_nogueira13@hotmail.com>
2023-03-04 00:07:39 -0300

    Implementação da reserva controller com base no cliente controller

--- src/main/java/com/gerenciador/repository/ReservaRepository.java
+++ src/main/java/com/gerenciador/repository/ReservaRepository.java
@@ -1,7 +1,6 @@
 package com.gerenciador.repository;
 
-import com.gerenciador.dto.reserva.Reserva;
-import com.gerenciador.model.Cliente;
+import com.gerenciador.model.Reserva;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReservaRepository extends CrudRepository <Reserva, Integer> {
