c8cea89e8dd171f370faf8b5278cdb6f6a70527a
rmmatieli <ramommatieli@hotmail.com>
2023-03-06 21:04:09 -0300

    Criacao do arquivo ClienteService para interacao com banco de dados.

---
+++ src/main/java/com/gerenciador/service/ClienteService.java
@@ -0,0 +1,38 @@
+package com.gerenciador.service;
+
+import com.gerenciador.model.Cliente;
+import com.gerenciador.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public Cliente cadastrar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listar(){
+        List<Cliente> lista = new ArrayList<>();
+        clienteRepository.findAll().forEach(lista::add);
+        return lista;
+    }
+
+    public Cliente atualizar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente excluir(int id){
+        Cliente cliente = clienteRepository.findById(id).orElseThrow();
+        clienteRepository.deleteById(id);
+        return cliente;
+    }
+
+
+}
